2918fd40f3142149e501cffb6c010b79f503ab15
Anıl <cazorte@gmail.com>
2022-05-31 13:59:23 +0300

    until step4 completed(quantity dropdoen selection didnt work)

--- src/test/java/com/cydeo/tasks/reviewTasks1/CartTest1.java
+++ src/test/java/com/cydeo/tasks/reviewTasks1/CartTest1.java
@@ -1,18 +1,66 @@
 package com.cydeo.tasks.reviewTasks1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import java.security.Key;
 
 public class CartTest1 {
 
-    public static void main(String[] args) {
+    @Test
+    public void TC1(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://www.amazon.com");
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 
+        String testText = ConfigurationReader.getProperty("searchText");
+        searchBox.sendKeys(testText + Keys.ENTER);
+
+        WebElement firstProduct = driver.findElement(By.xpath("(//img[@class = 's-image'])[1]"));
+        firstProduct.click();
+
+        //WebElement quantitySelection = driver.findElement(By.id("a-autoid-9-announce"));
+        //WebElement quantitySelection = driver.findElement(By.xpath("//span[@id='a-autoid-9']"));
+
+        WebElement quantitySelection = driver.findElement(By.xpath("//div[@id=\"selectQuantity\"]"));
+        quantitySelection.click();
+        quantitySelection.click();
+
+        //WebElement selection2 = driver.findElement(By.xpath("//span[@id='a-autoid-9']"));
+
+
+        WebElement selection2 = driver.findElement(By.xpath("//li[@aria-labelledby = 'quantity_1']"));
+        selection2.click();
+
+        WebElement addToChart = driver.findElement(By.id("add-to-cart-button"));
+        addToChart.click();
+
+//        WebElement itemText = driver.findElement(By.xpath("//span[@id='sw-subtotal-item-count']"));
+//        String actualItemText1 = itemText.getText();
+//        //System.out.println("actualItemText1 = " + actualItemText1);
+//        String expectedItemText1 = "Cart subtotal (2 items):";
+//
+//        Assert.assertEquals(actualItemText1,expectedItemText1);
+//        System.out.println("expectedItemText1 = " + expectedItemText1);
+
+        WebElement priceItem1 = driver.findElement(By.xpath("//span[.='$39.98']"));
+        String actualPriceItem1Text = priceItem1.getText();
+        String expectedPriceItem1Text = "$39.98";
+
+        Assert.assertEquals(actualPriceItem1Text,expectedPriceItem1Text);
+        System.out.println("expectedPriceItem1Text = " + expectedPriceItem1Text);
+
+        WebElement addToChartButton = driver.findElement(By.xpath("//a[@href='/gp/cart/view.html?ref_=sw_gtc']"));
+        addToChartButton.click();
 
 
 
 
     }
-
-
 }
